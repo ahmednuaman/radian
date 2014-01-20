99ce98c05705187e2943aca3e5075b717cbb1e72
Ahmed Nuaman <ahmed@ahmednuaman.com>
2014-01-20 11:24:01 +0000

    sorted js version

--- assets/javascript/controller/radian-controller.js
+++ assets/javascript/controller/radian-controller.js
@@ -3,11 +3,6 @@ define([
   'helper/radian-module-helper'
 ], function(_, helper) {
   return function(name, deps, funcs) {
-    klass = function() {};
-
-    _.assign(klass.prototype, funcs);
-
-    helper.registerController([name, deps, klass]);
-    helper.construct(klass, deps);
+    helper.registerController(arguments);
   };
 });
--- assets/javascript/helper/radian-module-helper.js
+++ assets/javascript/helper/radian-module-helper.js
@@ -4,14 +4,23 @@ define([
   'lodash'
 ], function(cfg, A, _) {
   var helper = {
-    construct: function(module, args) {
-      _.forEach(module.constructor.$inject, function(key, i) {
-        return module[key] = args[i];
-      });
+    construct: function(funcs) {
+      function Klass() {
+        var args = _.toArray(arguments),
+            that = this;
 
-      if (typeof module.init === 'function') {
-        module.init();
+        _.forEach(Klass.$inject, function(key, i) {
+          that[key] = args[i];
+        });
+
+        if (typeof this.init === 'function') {
+          this.init();
+        }
       }
+
+      _.assign(Klass.prototype, funcs);
+
+      return Klass;
     },
 
     inject: function(module, deps) {
@@ -21,6 +30,24 @@ define([
     register: function(type, name, deps, module) {
       helper.inject(module, deps);
       A.module(cfg.ngApp)[type](name, module);
+    },
+
+    registerClass: function(type) {
+      return function(args) {
+        args[2] = helper.construct(args[2]);
+
+        helper.registerModule(args, type);
+      };
+    },
+
+    registerFunction: function(type) {
+      return function(args) {
+        helper.registerModule(args, type);
+      };
+    },
+
+    registerModule: function(args, type) {
+      helper.register.apply(this, [type].concat(_.toArray(args)));
     },
 
     uppercaseFirstLetter: function(str) {
@@ -35,9 +62,7 @@ define([
     'filter',
     'service'
   ], function(type) {
-    helper['register' + helper.uppercaseFirstLetter(type)] = function(args) {
-      helper.register.apply(this, [type].concat(_.toArray(args)));
-    };
+    helper['register' + helper.uppercaseFirstLetter(type)] = type === 'controller' || type === 'service' ? helper.registerClass(type) : helper.registerFunction(type);
   });
 
   return helper;
